ca9a59961166a95ba3cbd97c5c786a6e234794e9
Marc <marc328@gmx.de>
2016-03-17 14:27:06 +0100

    some code improvement and comments added

--- arduino_spot_welder/arduino_spot_welder.ino
+++ arduino_spot_welder/arduino_spot_welder.ino
@@ -1,14 +1,13 @@
 // Arduino Battery Spot Welder
 // Project Page: http://www.instructables.com/id/DIY-Arduino-Battery-Spot-Welder/
 
-int mosfetPin = 5;
-int ledPin = 4;
-int tasterPin = 2;
-int sensorPin = 7;
-int sensorValue = 0;
-int pulse_time;
-int pulse_short;
-int pulse_long;
+int mosfetPin = 5;  //pin that controlls the mosfet drivers
+int ledPin = 4;   //pin to control the status led
+int tasterPin = 2;  //pin to trigger a pulse with the foot switch
+int sensorPin = A7;  //pin where the potentiometer value is read
+int sensorValue = 0;  //initalize sensor value with zero
+int pulse_time;   //variable to store the pulse time
+int pulse_short;  //variable to store the short pulse time
 
 
 //            0           1          2          3          4          5          6          7          8          9          .
@@ -22,16 +21,21 @@ void setup() {
 pinMode(tasterPin,INPUT);  // set tasterPin as Input
 pinMode(mosfetPin,OUTPUT);  // set mosfetPin as Output
 pinMode(ledPin,OUTPUT);  // set ledPin as Output
+pinMode(sensorPin,INPUT);  // set sensorPin as Input
 
 // 7 Segment Pins
 for (int i=6; i <= 13; i++) // 6-13 are set as Outputs
     pinMode(i,OUTPUT);
 
 
-//Flash the led once to verify correct placement
+//Light up the led and the display 2 seconds to verify correct placement and function
   digitalWrite(ledPin, HIGH);
-  delay(500);
+  for (int i=6; i <= 13; i++) // 6-13 are set to High which means all segments on
+    digitalWrite(i, HIGH);
+  delay(2000);
   digitalWrite(ledPin, LOW);
+  for (int i=6; i <= 13; i++) // 6-13 are set to High which means all segments on
+    digitalWrite(i, LOW);
 
 }
 
@@ -100,12 +104,11 @@ segmente(z[10]);
 //-------------------------------------------------------------------------------------------
 
 void DoppelImpuls(){  
-  sensorValue = analogRead(sensorPin);
  
-  pulse_long  = (sensorValue/51.15);   //makes a pulse of max 20 mS because SensorValue can max become 1023
-  pulse_short = (pulse_long/8);     // Impuls with 1/8 of the time of pulse_long
+  pulse_time = analogRead(sensorPin)/51.15;  //makes a pulse of max 20 mS because SensorValue can max become 1023
+  pulse_short = (pulse_time/8);     // Impuls with 1/8 of the time of pulse_time
  
-  if (pulse_short < 1)
+  if (pulse_short < 1) //makes the short pulse to be at least one milli second 
      pulse_short = 1; 
  
   digitalWrite(ledPin, HIGH);
@@ -114,7 +117,7 @@ void DoppelImpuls(){
   digitalWrite(mosfetPin, LOW);
   delay(pulse_short);      
     digitalWrite(mosfetPin, HIGH);
-  delay(pulse_long);   
+  delay(pulse_time);   
   digitalWrite(mosfetPin, LOW);
   delay(sensorValue);
   digitalWrite(ledPin, LOW);
